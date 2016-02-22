c56899b6e29d6d48c8896cce9f5c3c84547b41df
Milan Pavlik <pavlik.mil@gmail.com>
2016-02-22 14:33:48 +0000

    Add Travis config

---
+++ test/login/LoginActionTypes.spec.js
@@ -0,0 +1,15 @@
+import expect from 'expect.js';
+import { LOGIN, LOGOUT } from '../../src/login/LoginActionTypes';
+
+
+describe('LoginActionTypes', () => {
+
+    it('should have a LOGIN', () => {
+        expect(LOGIN).to.be.eql('LOGIN');
+    })
+
+    it('should have a LOGOUT', () => {
+        expect(LOGOUT).to.be.eql('LOGOUT');
+    })
+
+})
