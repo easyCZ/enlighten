58e9b22acad530a300faffd729f3f04dbffe87e7
Milan Pavlik <pavlik.mil@gmail.com>
2016-03-05 18:53:37 +0000

    Fix Reducer reference

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import { createStore } from 'redux';
-import rootReducer from '../reducers';
+import rootReducer from '../Reducers';
 
 function configureStore(initialState) {
   const store = createStore(rootReducer, initialState,
